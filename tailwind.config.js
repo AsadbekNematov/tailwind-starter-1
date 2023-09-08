57338070db4a1da743812d6cdb0c2ebf559d7a89
Asadbek <asadbekn443@gmail.com>
2023-09-08 04:44:00 -0400

    Modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
 module.exports = {
-  content: [],
+  content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: ["./dist/*.{html,js}"],
+  plugins: [],
 }
 
